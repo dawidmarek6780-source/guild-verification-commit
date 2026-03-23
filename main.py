cfdeb75ea58a22f3445199b1466631aa101fed97
dawidmarek6780-source <dawidmarek6780@gmail.com>
2026-03-23 21:15:41 +0100

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-23T20:15:36.690Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-23T20:15:41.196Z
+Commit: Fix CSS styling bug on mobile devices
