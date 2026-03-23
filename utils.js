3491a11d2180eb69bb425c93f31ec6b55fa3987d
dawidmarek6780-source <dawidmarek6780@gmail.com>
2026-03-23 21:13:34 +0100

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-23T20:12:23.229Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-23T20:13:34.118Z
+Commit: Resolve null pointer exception in user service
