3b05746f61c81e980d12279ab0e2ae4dd699edc7
dawidmarek6780-source <dawidmarek6780@gmail.com>
2026-03-23 21:12:18 +0100

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-23T20:12:02.006Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-23T20:12:17.827Z
+Commit: Patch API endpoint error handling
