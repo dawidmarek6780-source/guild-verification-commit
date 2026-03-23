327d44f7f19b5d1f003e849b572a698e97c32285
dawidmarek6780-source <dawidmarek6780@gmail.com>
2026-03-23 21:13:45 +0100

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-23T20:13:38.991Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-23T20:13:45.430Z
+Commit: Resolve null pointer exception in user service
