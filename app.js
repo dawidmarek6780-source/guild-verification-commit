6a6959a879b9a25545556e6de04f25cb97dc6a89
dawidmarek6780-source <dawidmarek6780@gmail.com>
2026-03-23 21:13:58 +0100

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-23T20:13:57.002Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-23T20:13:58.257Z
+Commit: Resolve null pointer exception in user service
